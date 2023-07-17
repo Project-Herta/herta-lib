c99ef9bdefed1ef5b26ce8ec4f3f6e1549ac49c6
Kiwifuit <mahkiwi123@gmail.com>
2023-07-17 13:18:32 +0800

    Added Enemy ERES and RES

--- src/extractor/enemy.rs
+++ src/extractor/enemy.rs
@@ -1,6 +1,6 @@
 use super::{parse_html, parse_url};
 use crate::url::get_original_image;
-use scraper::Selector;
+use scraper::{Html, Selector};
 
 pub struct Enemy {
     pub link: String,
@@ -31,6 +31,48 @@ pub fn index_enemies(html: String) -> Vec<Enemy> {
         .map(|e| Enemy {
             link: e.value().attr("href").unwrap().to_string(),
             name: e.value().attr("title").unwrap().to_string(),
+        })
+        .collect::<Vec<_>>()
+}
+
+pub fn get_enemy_resistances(html: String) -> Vec<f32> {
+    let html = parse_html(html);
+    let table_selector = Selector::parse("table.wikitable").unwrap();
+    let row_selector = Selector::parse("tr").unwrap();
+    let col_selector = Selector::parse("td").unwrap();
+
+    select_table(0, html, &table_selector, &row_selector, &col_selector)
+}
+pub fn get_enemy_effect_resistances(html: String) -> Vec<f32> {
+    let html = parse_html(html);
+    let table_selector = Selector::parse("table.wikitable").unwrap();
+    let row_selector = Selector::parse("tr").unwrap();
+    let col_selector = Selector::parse("td").unwrap();
+
+    select_table(1, html, &table_selector, &row_selector, &col_selector)
+}
+
+fn select_table(
+    n: usize,
+    html: Html,
+    table_selector: &Selector,
+    row_selector: &Selector,
+    col_selector: &Selector,
+) -> Vec<f32> {
+    html.select(&table_selector)
+        .nth(n)
+        .unwrap()
+        .select(&row_selector)
+        .nth(2)
+        .unwrap()
+        .select(&col_selector)
+        .map(|e| {
+            e.inner_html()
+                .strip_suffix("%")
+                .unwrap()
+                .parse::<f32>()
+                .unwrap()
+                / 100.0
         })
         .collect::<Vec<_>>()
 }
