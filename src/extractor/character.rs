981c7bd951f9d11c50d7e854e63c4d908a44427b
Kiwifuit <mahkiwi123@gmail.com>
2023-08-16 23:34:01 +0800

    Removed character art because this is the backend not the front

--- src/extractor/character.rs
+++ src/extractor/character.rs
@@ -13,8 +13,6 @@ pub struct Character {
     pub path_image: String,
     pub ctype: String,
     pub ctype_image: String,
-    pub portrait: Option<String>,
-    pub splash: Option<String>,
 }
 
 pub fn index_characters(html: String) -> Vec<Character> {
@@ -76,8 +74,6 @@ pub fn index_characters(html: String) -> Vec<Character> {
             path_image: path_image.to_string(),
             ctype: ctype.to_string(),
             ctype_image: ctype_image.to_string(),
-            portrait: None,
-            splash: None,
         })
     }
 
