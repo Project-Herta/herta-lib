f7fddb16dced98524389ee5903e0bad521a90d87
Kiwifuit <mahkiwi123@gmail.com>
2023-08-18 12:36:27 +0800

    Made `config` functions public

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,6 +1,8 @@
+mod config;
+
+pub use config::*;
 use platform_dirs::AppDirs;
 use std::path::PathBuf;
-mod config;
 
 pub fn get_root_dir(name: &str, subdir: Option<String>) -> PathBuf {
     let dir = AppDirs::new(Some(name), false).unwrap().data_dir;
